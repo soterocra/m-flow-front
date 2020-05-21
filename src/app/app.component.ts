229f46e7cbd4f41056b1af0fa0ea4d358d2b39bb
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2020-05-21 19:54:58 -0300

    Adicionado menu de hamburger com seta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'mflow-chat';
 
+  menuOpen = true;
+
   selectedIndex: number = -1;
   selectedContact: any = {};
 
